d6925f449170c2c5d41b7d9ed9e4e74f1352ce34
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-05-18 17:16:16 -0300

    tipos primitivos

---
+++ Leccion02/tipoPrimitivo/src/TipoPrimitivo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pia Olivera
+ */
+public class TipoPrimitivo {
+    /*
+        tipos primitivos enteros: byte, short, int, long
+    */
+    public static void main(String[] args) {
+        byte numeroByte = 10;
+        System.out.println("valor minimo byte: "+Byte.MIN_VALUE);
+        System.out.println("Valor maximo: "+Byte.MAX_VALUE);
+        
+        
+        short numeroShort = 10;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Numero minimo short: " +Short.MIN_VALUE);
+        System.out.println("Numero Maximo short: " +Short.MAX_VALUE);
+        
+        int numeroInt = 10;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Valor minimo int: "+Integer.MIN_VALUE);
+        System.out.println("Valor maximo int: "+Integer.MAX_VALUE);
+        
+    }
+    
+}
