57107d7be22fcca7a19be455e7f3d141b4ed600e
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-05-31 17:38:50 -0300

    paso por valor, paso por referencia

--- Leccion08/HolaMundoJava/src/clases/PruebaPersona.java
+++ Leccion08/HolaMundoJava/src/clases/PruebaPersona.java
@@ -1,22 +1,20 @@
 package clases;
 
-/**
- *
- * @author Pia Olivera
- */
 public class PruebaPersona {
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona();
+        Persona persona1 =  new Persona(); 
         persona1.nombre = "Juan";
         persona1.apellido = "Perez";
-        
         persona1.desplegarInformacion();
         
         Persona persona2 = new Persona();
-        persona2.nombre = "Carlos";
-        persona2.apellido = "Bustamante";
         
+        System.out.println("persona1 = " + persona1);
+        System.out.println("persona2 = " + persona2);
+        
+        persona2.desplegarInformacion();
+        persona2.nombre = "Karla";
+        persona2.apellido = "Lara";
         persona2.desplegarInformacion();
     }
 }
