98793e0d13254fbc119d0bab5a074d1dc54358a2
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-06-02 17:03:46 -0300

    Return y null

--- Leccion08/HolaMundoJava/src/pasoporreferencia/PasoPorReferencia.java
+++ Leccion08/HolaMundoJava/src/pasoporreferencia/PasoPorReferencia.java
@@ -4,14 +4,20 @@ import clases.Persona;
 
 public class PasoPorReferencia {
     public static void main(String[] args) {
-        Persona persona1 = new Persona();
-        persona1.nombre = "Juan";
-        System.out.println("persona1 nombre: " + persona1.nombre);
-        cambiarValor(persona1);
-        System.out.println("persona1 cambio nombre:" + persona1.nombre);
+       // Persona persona1 = new Persona();
+//        persona1.nombre = "Juan";
+//        System.out.println("persona1 nombre: " + persona1.nombre);
+        Persona persona1 = null;
+        persona1 = cambiarValor(persona1);
+       // System.out.println("persona1 cambio nombre:" + persona1.nombre);
     }
     
-    public static void cambiarValor(Persona persona){
+    public static Persona cambiarValor(Persona persona){
+        if(persona == null){
+            System.out.println("Valor de persona invalido: null");
+            return null;
+        }
         persona.nombre = "Karla";
+        return persona;
     }
 }
