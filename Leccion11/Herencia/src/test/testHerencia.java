e6ebb26db2900220ca808cfb9279d8ef03681a11
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-06-09 22:24:54 -0300

    Contexto estatico

---
+++ Leccion11/Herencia/src/test/testHerencia.java
@@ -0,0 +1,14 @@
+package test;
+
+import domain.Empleado;
+
+/**
+ *
+ * @author Pia Olivera
+ */
+public class testHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 5000.0);
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
