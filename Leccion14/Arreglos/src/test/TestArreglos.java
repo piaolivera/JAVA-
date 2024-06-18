c1cdb30af5c838d27aa218984697411f3eff8aba
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-06-18 12:31:53 -0300

    Array

--- Leccion14/Arreglos/src/test/TestArreglos.java
+++ Leccion14/Arreglos/src/test/TestArreglos.java
@@ -8,5 +8,18 @@ public class TestArreglos {
     public static void main(String[] args) {
         int edades[] = new int[3];
         System.out.println("edades = " + edades);
+        
+        
+        edades[0] =10;
+        System.out.println("edades 0 = " + edades[0]);
+        
+        edades[1] =5;
+        edades[2] = 12;
+        System.out.println("edades 1 = " + edades[1]);
+        System.out.println("edades 2 = " +edades[2]);
+        
+        for(int i =0; i< edades.length; i++){
+            System.out.println("Recorremos el arreglo: "+i +": "+edades[i]);
+    }
     }
 }
