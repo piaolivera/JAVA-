6692537db4b23ddd6c008a5486d3c519ce2a2781
Pía Olivera <107521407+piaolivera@users.noreply.github.com>
2024-09-13 15:45:21 -0300

    app zona fit

---
+++ zona_fit/src/main/java/po/zona_fit/ZonaFitApplication.java
@@ -0,0 +1,13 @@
+package po.zona_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonaFitApplication.class, args);
+	}
+
+}
